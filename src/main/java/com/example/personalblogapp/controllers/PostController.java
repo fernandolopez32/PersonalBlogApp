2ab07736ac9888b62b1949a279d49cf4d4eb8d3b
Fernando Lopez <flopez00032@gmail.com>
2022-12-07 13:29:52 -0600

    adding the partials directory

--- src/main/java/com/example/personalblogapp/controllers/PostController.java
+++ src/main/java/com/example/personalblogapp/controllers/PostController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PostController {
 
-    @GetMapping("/index")
+    @GetMapping("/home")
     public String index(){
-        return "index";
+        return "home";
     }
 
 }
