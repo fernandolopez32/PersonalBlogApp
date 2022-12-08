31e16e3f6065e4ce3122be916533c64f7c774bd6
Fernando Lopez <flopez00032@gmail.com>
2022-12-08 09:02:26 -0600

    added method in the Post repository

--- src/main/java/com/example/personalblogapp/repositories/PostRepository.java
+++ src/main/java/com/example/personalblogapp/repositories/PostRepository.java
@@ -1,7 +1,11 @@
 package com.example.personalblogapp.repositories;
 
 import com.example.personalblogapp.models.Post;
+import com.example.personalblogapp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository <Post, Long> {
+    Post findById(long id);
+    Post findByUser(User user);
+
 }
