53ac0b6a2d22a66c3dc9405a3253fa173caa47cf
Fernando Lopez <flopez00032@gmail.com>
2022-12-27 22:45:04 -0600

    added post maping for home

--- src/main/java/com/example/personalblogapp/controllers/PostController.java
+++ src/main/java/com/example/personalblogapp/controllers/PostController.java
@@ -1,14 +1,22 @@
 package com.example.personalblogapp.controllers;
 
+import com.example.personalblogapp.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PostController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute(new Post());
         return "home";
     }
 
+    @PostMapping("createPost")
+    public String createPost(){
+        return "redirect:/";
+    }
 }
